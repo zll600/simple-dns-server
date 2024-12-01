098a1347fb6a4b1f1db5fb51f1dd5a9a9bc11924
zll600 <3400692417@qq.com>
2024-12-02 07:20:19 +0800

    example 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple DNS Server
+
+# Acknowledge
+
+- https://github.com/EmilHernvall/dnsguide
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::{fs::File, io::{Read, Result}};
+
+use byte_packet_buffer::BytePacketBuffer;
+use dns_packet::DnsPacket;
+use types::Error;
+use types::Result;
+
+pub mod types;
+pub mod query_type;
+pub mod result_code;
+pub mod dns_header;
+pub mod dns_question;
+pub mod dns_record;
+pub mod byte_packet_buffer;
+pub mod dns_packet;
+
 fn main() {
-    println!("Hello, world!");
+    fn main() -> Result<()> {
+        let mut f = File::open("response_packet.txt")?;
+        let mut buffer = BytePacketBuffer::new();
+        f.read(&mut buffer.buf)?;
+
+        let packet = DnsPacket::from_buffer(&mut buffer)?;
+        println!("{:#?}", packet.header);
+
+        for q in packet.questions {
+            println!("{:#?}", q);
+        }
+        for rec in packet.answers {
+            println!("{:#?}", rec);
+        }
+        for rec in packet.authorities {
+            println!("{:#?}", rec);
+        }
+        for rec in packet.resources {
+            println!("{:#?}", rec);
+        }
+
+        Ok(())
+    }
 }
