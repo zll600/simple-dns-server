6f230010ec939283991bc196f70cbad98b663d63
zll600 <3400692417@qq.com>
2024-12-02 20:59:06 +0800

    complete exmaple1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::{fs::File, io::{Read, Result}};
+use std::{fs::File, io::Read as _};
 
 use byte_packet_buffer::BytePacketBuffer;
 use dns_packet::DnsPacket;
-use types::Error;
 use types::Result;
 
 pub mod types;
@@ -14,28 +13,26 @@ pub mod dns_record;
 pub mod byte_packet_buffer;
 pub mod dns_packet;
 
-fn main() {
-    fn main() -> Result<()> {
-        let mut f = File::open("response_packet.txt")?;
-        let mut buffer = BytePacketBuffer::new();
-        f.read(&mut buffer.buf)?;
+fn main() -> Result<()> {
+    let mut f = File::open("response_packet.txt")?;
+    let mut buffer = BytePacketBuffer::new();
+    f.read(&mut buffer.buf)?;
 
-        let packet = DnsPacket::from_buffer(&mut buffer)?;
-        println!("{:#?}", packet.header);
+    let packet = DnsPacket::from_buffer(&mut buffer)?;
+    println!("{:#?}", packet.header);
 
-        for q in packet.questions {
-            println!("{:#?}", q);
-        }
-        for rec in packet.answers {
-            println!("{:#?}", rec);
-        }
-        for rec in packet.authorities {
-            println!("{:#?}", rec);
-        }
-        for rec in packet.resources {
-            println!("{:#?}", rec);
-        }
-
-        Ok(())
+    for q in packet.questions {
+        println!("{:#?}", q);
     }
+    for rec in packet.answers {
+        println!("{:#?}", rec);
+    }
+    for rec in packet.authorities {
+        println!("{:#?}", rec);
+    }
+    for rec in packet.resources {
+        println!("{:#?}", rec);
+    }
+
+    Ok(())
 }
